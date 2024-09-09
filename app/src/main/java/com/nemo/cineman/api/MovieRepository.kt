0aece10bf986a9f8a87d08064dde12f51d2457fe
MINH2201 <thaiminhnguyen2003@gmail.com>
2024-09-09 14:42:08 +0700

    feat: implement movie repository

--- app/src/main/java/com/nemo/cineman/api/MovieRepository.kt
+++ app/src/main/java/com/nemo/cineman/api/MovieRepository.kt
@@ -1,7 +1,14 @@
 package com.nemo.cineman.api
 
+import com.nemo.cineman.entity.Movie
+import com.nemo.cineman.entity.MovieDao
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(
-    val service: MovieService
-)
+    private val service: MovieService,
+    private val movieDao: MovieDao
+) {
+    fun getLocalMovie() : List<Movie> {
+       return movieDao.getAllMovie()
+    }
+}
--- app/src/main/java/com/nemo/cineman/di/module/AppModule.kt
+++ app/src/main/java/com/nemo/cineman/di/module/AppModule.kt
@@ -1,9 +1,14 @@
 package com.nemo.cineman.di.module
 
+import android.content.Context
+import androidx.room.Room
 import com.nemo.cineman.api.MovieService
+import com.nemo.cineman.entity.MovieDao
+import com.nemo.cineman.room.AppDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -21,7 +26,11 @@ object AppModule {
     @Singleton
     fun provideMovieService(retrofit: Retrofit): MovieService = retrofit.create(MovieService::class.java)
 
-//    @Provides
-//    @Singleton
-//    fun provideDb()
+    @Provides
+    @Singleton
+    fun provideDb(@ApplicationContext context: Context): AppDatabase = Room.databaseBuilder(context, AppDatabase::class.java, "app_db").build()
+
+    @Provides
+    @Singleton
+    fun provideMovieDao(appDb: AppDatabase) = appDb.movieDao()
 }
