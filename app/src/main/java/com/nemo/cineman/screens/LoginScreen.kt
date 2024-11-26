907b2eadf968acc8ac33e341809fce16b1780d3e
MINH2201 <thaiminhnguyen2003@gmail.com>
2024-11-26 22:29:12 +0700

    feat: implement LoginScreen

--- app/src/main/java/com/nemo/cineman/screens/LoginScreen.kt
+++ app/src/main/java/com/nemo/cineman/screens/LoginScreen.kt
@@ -1,33 +1,48 @@
 package com.nemo.cineman.screens
 
+import android.content.Context
+import android.net.Uri
 import android.util.Log
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.nemo.cineman.ui.theme.playwrite
+import com.nemo.cineman.viewmodel.AuthViewModel
 
 @Composable
-fun LoginScreen(navController: NavController){
+fun LoginScreen(
+    navController: NavController,
+
+){
     Column(
         modifier = Modifier
             .fillMaxHeight()
@@ -64,53 +79,99 @@ fun WelcomeText(){
 }
 
 @Composable
-fun LoginForm(navController: NavController){
+fun LoginForm(
+    navController: NavController,
+    viewModel: AuthViewModel = hiltViewModel()
+    ) {
     var userText by remember {
         mutableStateOf("")
     }
     var passwordText by remember {
         mutableStateOf("")
     }
-    Column {
-        TextField(
-            value = userText,
-            onValueChange = { userText = it },
-            label = { Text("Username") },
+    val isLoading by viewModel.isLoading.observeAsState(false)
+    val url = "https://www.themoviedb.org/authenticate/${viewModel.requestToken}"
+    val context = LocalContext.current
+//    LaunchedEffect(isLoading) {
+//        if(!isLoading && viewModel.requestToken != null){
+//            Log.d("MyLog", viewModel.requestToken)
+//            openUrlInCustomTab(context, url)
+//        }
+//    }
+
+    if (isLoading) {
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp)
-        )
-        TextField(
-            value = passwordText,
-            onValueChange = { passwordText = it },
-            label = { Text("Password") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp)
-        )
-        FilledTonalButton(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(10.dp),
-            onClick = {
-                Log.d("MyLog", "Login pressed")
-                navController.navigate("menu") }
+                .fillMaxSize()
         ) {
-            Text(
-                text = "Login"
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center),
+                color = MaterialTheme.colorScheme.primary
             )
         }
-        TextButton(onClick = {
-            Log.d("MyLog", "No account pressed")
-        }) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = "Don't have account? Register now",
-                textAlign = TextAlign.Center
+
+    } else {
+        Column {
+            TextField(
+                value = userText,
+                onValueChange = { userText = it },
+                label = { Text("Username") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
             )
+            TextField(
+                value = passwordText,
+                onValueChange = { passwordText = it },
+                label = { Text("Password") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+            )
+            FilledTonalButton(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(10.dp),
+                onClick = {
+                    Log.d("MyLog", "Login pressed")
+                    navController.navigate("menu")
+                }
+            ) {
+                Text(
+                    text = "Login"
+                )
+            }
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(10.dp),
+                onClick = {
+                    viewModel.getRequestToken()
+                    if (viewModel.requestToken != null) {
+                        Log.d("MyLog", viewModel.requestToken)
+                        openUrlInCustomTab(context, url)
+                    }
+                }
+            ) {
+                Text(text = "Sign in using TMDB account")
+            }
+            TextButton(onClick = {
+                Log.d("MyLog", "No account pressed")
+            }) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Don't have account? Register now",
+                    textAlign = TextAlign.Center
+                )
+            }
+
         }
 
     }
+}
 
+fun openUrlInCustomTab(context: Context,url: String){
+    val customTabsIntent = CustomTabsIntent.Builder().build()
+    customTabsIntent.launchUrl(context, Uri.parse(url))
 }
 
--- app/src/main/java/com/nemo/cineman/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/nemo/cineman/viewmodel/AuthViewModel.kt
@@ -1,8 +1,17 @@
 package com.nemo.cineman.viewmodel
 
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.nemo.cineman.api.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -10,16 +19,33 @@ import javax.inject.Inject
 class AuthViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
-    private var requestToken : String = ""
+    private var _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> get() = _isLoading
+    var requestToken: String = null.toString()
+    private var _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
 
     fun getRequestToken() {
-        authRepository.getRequestToken { result ->
-            result.onSuccess { requestTokenResponse ->
-                requestToken = requestTokenResponse!!.requestToken
-                println("Get Request Token Successfully")
-            } .onFailure {
-                println("Get Request Token Failed")
+        viewModelScope.launch {
+            _isLoading.postValue(true)
+            Log.d("MyLog", isLoading.value.toString())
+            authRepository.getRequestToken { result ->
+                result.onSuccess { requestTokenResponse ->
+                    requestToken = requestTokenResponse!!.requestToken
+                    Log.d("MyLog", "Get Request Token Successfully")
+                    Log.d("MyLog", requestToken)
+                    println("Get Request Token Successfully")
+                    _isLoading.postValue(false)
+                    Log.d("MyLog", isLoading.value.toString())
+                }.onFailure {
+                    Thread.sleep(10000)
+                    _error.postValue("Get Request Token Failed")
+                    _isLoading.postValue(false)
+                    Log.d("MyLog", "Get Request Token Failed")
+                    println("Get Request Token Failed")
+                }
             }
+
         }
     }
 
