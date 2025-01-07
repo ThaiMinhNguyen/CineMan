e391e35e89cf844c8b6b4d062a8116c1462f08cb
MINH2201 <thaiminhnguyen2003@gmail.com>
2025-01-07 21:04:10 +0700

    feat: implement session handler

--- app/src/main/java/com/nemo/cineman/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/nemo/cineman/viewmodel/MainViewModel.kt
@@ -1,20 +1,29 @@
 package com.nemo.cineman.viewmodel
 
+import android.app.Notification
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.nemo.cineman.api.AuthRepository
 import com.nemo.cineman.api.MovieRepository
 import com.nemo.cineman.entity.Movie
-import com.nemo.cineman.entity.MovieCertification
+import com.nemo.cineman.entity.RequestTokenBody
+import com.nemo.cineman.entity.SessionResponse
+import com.nemo.cineman.entity.SharedPreferenceManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val movieRepository: MovieRepository
+    private val movieRepository: MovieRepository,
+    private val authRepository: AuthRepository,
+    private val sharedPreferenceManager: SharedPreferenceManager
 ) : ViewModel() {
     private val _nowPlayingMovies = MutableLiveData<List<Movie>>()
     val nowPlayingMovies: LiveData<List<Movie>> get() = _nowPlayingMovies
@@ -22,10 +31,62 @@ class MainViewModel @Inject constructor(
     private val _popularMovies = MutableLiveData<List<Movie>>()
     val popularMovies: LiveData<List<Movie>> get() = _popularMovies
 
+    private val _notificationEvent = MutableStateFlow<String?>(null)
+    val notificationEvent : StateFlow<String?> = _notificationEvent
 
-//    init {
-//        getMovies(1)
-//    }
+    fun onNotificationHandled(){
+        _notificationEvent.value = null
+    }
+
+    fun onLogOutHandled(){
+        sharedPreferenceManager.clearAll()
+        Log.d("MyLog", "Wipe out all sharedPreferences/LogOut")
+    }
+
+    suspend fun checkSession(){
+        val sessionId = sharedPreferenceManager.getSessionId()
+        if(sessionId == null){
+            val token = sharedPreferenceManager.getRequestToken()
+            if (token != null){
+                Log.d("MyLog", "checkSession: got request token: $token")
+                var attempts = 0
+                val maxRetries = 3
+                var result: Result<SessionResponse?>? = null
+                while (attempts < maxRetries && (result == null || result.isFailure == true)) {
+                    result = authRepository.getNewSession(token)
+                    attempts++
+                    if (result.isFailure) {
+                        delay(1000 * attempts.toLong())
+                    }
+                }
+                if (result?.isSuccess == true) {
+                    val newSession = result.getOrNull()
+                    newSession?.let {
+                        it.sessionId?.let { it1 -> sharedPreferenceManager.saveSession(it1) }
+                        Log.d("MyLog", "checkSession: New session created: ${it.sessionId}")
+                    }
+                } else {
+                    val exception = result?.exceptionOrNull()
+                    exception?.let {
+                        Log.e("MyLog", "checkSession: Error creating session: ${it.message}")
+                        // TODO: Quay lại trang login hoặc thông báo lỗi cho người dùng
+                        _notificationEvent.value = "Không thể tạo session."
+                    }
+                }
+
+            }
+
+        } else {
+            if(sharedPreferenceManager.isSessionExpired()){
+                _notificationEvent.value = "Session hết hạn. Vui lòng đăng nhập lại."
+                Log.e("MyLog", "checkSession: Session expired")
+            } else {
+                Log.e("MyLog", "checkSession: Session still valid: $sessionId")
+            }
+        }
+    }
+
+
 
     fun getNowPlayingMovies(page: Int) {
         viewModelScope.launch {
