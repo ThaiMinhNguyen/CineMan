8f4d8b67d19634ff1ba66d8e5e7d7c71675714de
MINH2201 <thaiminhnguyen2003@gmail.com>
2025-01-07 21:02:33 +0700

    feat: implement nav event

--- app/src/main/java/com/nemo/cineman/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/nemo/cineman/viewmodel/AuthViewModel.kt
@@ -1,21 +1,23 @@
 package com.nemo.cineman.viewmodel
 
-import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
 import com.nemo.cineman.api.AuthRepository
+import com.nemo.cineman.entity.SharedPreferenceManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val sharedPreferenceManager: SharedPreferenceManager
 ) : ViewModel() {
     private var _isLoading = MutableLiveData<Boolean>().apply { value = false }
     val isLoading: LiveData<Boolean> get() = _isLoading
@@ -23,6 +25,8 @@ class AuthViewModel @Inject constructor(
     val requestToken: LiveData<String> get() = _requestToken
     private var _error = MutableLiveData<String>()
     val error: LiveData<String> get() = _error
+    private val _navigationEvent = MutableStateFlow<String?>(null)
+    val navigationEvent: StateFlow<String?> = _navigationEvent
 
 
     suspend fun getRequestToken() : String? {
@@ -32,11 +36,14 @@ class AuthViewModel @Inject constructor(
             val result = authRepository.getRequestToken()
             result.getOrNull()?.requestToken?.let { token ->
                 val url = "https://www.themoviedb.org/authenticate/$token"
+                _requestToken.value = token
                 Log.d("MyLog", "Get Request Token Successfully")
                 Log.d("MyLog", "Token: $token")
                 Log.d("MyLog", "Requested Token On Success: $token")
                 Log.d("MyLog", "Requested Token when press button: " + "${requestToken.value}/$token")
                 Log.d("MyLog", url)
+                _navigationEvent.value = url
+                sharedPreferenceManager.saveToken(token)
                 return url
             } ?: run {
                 _error.postValue("Request token is null")
@@ -49,6 +56,12 @@ class AuthViewModel @Inject constructor(
         } finally {
             _isLoading.value = false
         }
+    }
+
+
+
+    fun onNavigationHandled() {
+        _navigationEvent.value = null
     }
 
 }
