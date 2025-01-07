eeb649dfc253b1cbcb00ae93313d7bfe52352ee5
MINH2201 <thaiminhnguyen2003@gmail.com>
2025-01-07 21:00:34 +0700

    feat: create SessionResponse class

--- app/src/main/java/com/nemo/cineman/entity/Authentication.kt
+++ app/src/main/java/com/nemo/cineman/entity/Authentication.kt
@@ -12,3 +12,10 @@ data class RequestTokenBody(
     @SerializedName("request_token") val requestToken: String
 )
 
+data class SessionResponse(
+    @SerializedName("success") val success: Boolean,
+    @SerializedName("session_id") val sessionId: String? = null,
+    @SerializedName("failure") val failure: Boolean? = null,
+    @SerializedName("status_code") val statusCode: Int? = null,
+    @SerializedName("status_message") val statusMessage: String? = null
+)
