821d0553fcd99580e552b5d051923fd615f95bed
MINH2201 <thaiminhnguyen2003@gmail.com>
2025-01-15 22:00:05 +0700

    feat: add bottom bar

--- app/src/main/java/com/nemo/cineman/screens/ListMovieScreen.kt
+++ app/src/main/java/com/nemo/cineman/screens/ListMovieScreen.kt
@@ -1,14 +1,25 @@
 package com.nemo.cineman.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -19,6 +30,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -34,40 +46,79 @@ fun ListMovieScreen(
     viewModel: MainViewModel = hiltViewModel(),
     type: ListType
 ) {
-
     val movies = viewModel.getMoviesByType(type).collectAsLazyPagingItems()
 
-    Column {
-        TopAppBar(
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                titleContentColor = Color.Black,
-            ),
-            title = {
-                Text(
-                    text = type.name + " Movies"
-                )
-            },
-            navigationIcon = {
-                IconButton(onClick = {
-                    navController.popBackStack()
-                    Log.d("MyLog", "Pop Back Stack")
-                } ) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Back button"
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = Color.Black,
+                ),
+                title = {
+                    Text(
+                        text = type.name + " Movies"
                     )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                        Log.d("MyLog", "Pop Back Stack")
+                    }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back button"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Menu trailing icon"
+                        )
+                    }
                 }
-            },
-            actions = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.Menu,
-                        contentDescription = "Menu trailing icon"
-                    )
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                IconButton(
+                    onClick = {
+                        navController.navigate("menu") {
+                            launchSingleTop = true
+                        }
+                    },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
+                }
+                IconButton(
+                    onClick = { /* Handle Home action */ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Home")
+                }
+                IconButton(
+                    onClick = { /* Handle Home action */ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(imageVector = Icons.Default.Person, contentDescription = "Home")
+                }
+                IconButton(
+                    onClick = { /* Handle Home action */ },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings")
                 }
             }
-        )
-        MovieColumn(movies = movies)
-    }
+        },
+        content = { paddingValues ->
+            Box(modifier = Modifier.padding(paddingValues)) {
+                MovieColumn(movies = movies)
+            }
+        }
+    )
+
+
 }
